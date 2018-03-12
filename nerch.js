3e587791cc3820d130e86f818ece21220284d7aa
nerrma <37285497+nerrma@users.noreply.github.com>
2018-03-12 21:52:46 +1100

    Update nerch.js

--- nerch.js
+++ nerch.js
@@ -1,26 +1,62 @@
 const SHA256 = require('crypto-js/sha256');
+class Address{
+    constructor(pk){
+        let block = new Block();
+        this.addressChain = [];
+        this.public = this.addressHash();
+    }
+    createNewAddress(){
+        this.addressChain.push(new Address(this.addressChain.length));
+    }
+    addressHash(){
+        return SHA256(this.pk).toString();
+    }
+}
 
+class Transaction{
+    constructor(to, from, amount){
+        this.to = to;
+        this.from = from;
+        this.amount = amount;
+    }
+}
 class Block{
     constructor(date, info, prevHash = ''){
         this.date = date;
         this.info = info;
         this.prevHash = prevHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
     calculateHash(){
-        return SHA256(this.date + this.info + this.prevHash).toString();
+            return SHA256(this.date + this.info + this.prevHash + this.nonce).toString();
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+    console.log("Block mined: " + this.hash);
+    console.log("Info: " + this.info);
     }
 }
 class Blockchain{
     constructor(){
         this.chain = [this.createGensisBlock()];
+        this.pendingtrans = [];
+        this.difficulty = 2;
     }
     createGensisBlock(){
         return new Block(Date.now(), "First block", null);
     }
     addBlock(info){
         this.info = info;
-        this.chain.push(new Block(Date.now(), info, this.chain[this.chain.length - 1].calculateHash()));
+        let block = new Block(Date.now(), info, this.chain[this.chain.length - 1].calculateHash());
+        block.mineBlock(this.difficulty);
+        this.chain.push(block);
+    }
+    createTransaction(Transaction){
+        this.pendingtrans.push(Transaction);
     }
     generateBlock(blockno){
         this.blockno = blockno;
@@ -33,5 +69,6 @@ class Blockchain{
 }
 
 let coin = new Blockchain();
-coin.generateBlock(5);
-console.log(coin.chain);
+let address = new Address();
+coin.addBlock(3);
+console.log();
