a0181372010d740dc706e9010e75ab6c7f55a837
nerrma <37285497+nerrma@users.noreply.github.com>
2018-03-30 20:29:58 +1100

    Update nerch.js

--- nerch.js
+++ nerch.js
@@ -1,9 +1,9 @@
 const SHA256 = require('crypto-js/sha256');
 class Address{
-    constructor(username, index){
+    constructor(username, index, balance){
         this.publicKey = this.hash(username);
-        this.privateKey = this.hash(this.publickey); 
         this.index = index;
+        this.balance = balance;
     }
     hash(obj){
         return SHA256(obj).toString();
@@ -17,16 +17,20 @@ class Addresses{
     hash(obj){
         return SHA256(obj).toString();
     }
-    newAddress(name){
-        this.chain.push(new Address(name, this.i));
+    newAddress(name, balance){
+        this.chain.push(new Address(name, this.i, balance));
         this.i++;
     }
-    search(publickey){
+    AD(publickey){
         for(this.i = 0; this.i < this.chain.length; this.i++){
             if(this.chain[this.i].publicKey == publickey){
-                return this.chain[this.i].index; 
+                return this.chain[this.i]; 
             }
         }
+    }
+   
+    getBalanceOfAddress(address){
+        return ("\nThe balance of " + address.publicKey + " is " + address.balance);
     }
 }
 
@@ -73,6 +77,7 @@ class Blockchain{
     minePendingTransactions(mineaddress){
             let block = new Block(Date.now(), this.pendingtrans);
             block.mineBlock(this.difficulty);
+            this.processTransactions();
             this.chain.push(block);
             this.pendingtrans = [new Transaction(mineaddress, null, this.minereward)];
             if(this.checkHold() == true){
@@ -83,27 +88,12 @@ class Blockchain{
                 this.hold = [];
             }
     }
-
-    getBalanceOfAddress(address){
-        let balance = 0;
-        
-        for(const block of this.chain){
-            for(const trans of block.info){
-                if(trans.from === address){
-                    balance -= trans.amount;
-                }
-                if(trans.to === address){
-                    balance += trans.amount;
-                }
-            }
-        }
-        return ("The balance of " + address + " is " + balance);
-    }
+  
     createTransaction(Transaction){
-        if(this.getBalanceOfAddress(Transaction.from.publicKey) <= 0){
-            return false;
-        }
-        else if(this.getBalanceOfAddress(Transaction.from.publicKey) < 0){
+        if(Transaction.from.balance <= 0){
+             return false;
+         }
+        else if(Transaction.from.balance > 0){
             if(this.pendingtrans.length < this.blocksize){
                 this.pendingtrans.push(Transaction);
             }
@@ -120,14 +110,20 @@ class Blockchain{
             return true;
         }
     }
+    processTransactions(){
+        for(this.i = 0; this.i < this.pendingtrans.length; this.i++){
+            this.pendingtrans[this.i].to.balance += this.pendingtrans[this.i].amount;
+            this.pendingtrans[this.i].from.balance -= this.pendingtrans[this.i].amount;
+        }
+    }
 }
 
 let coin = new Blockchain();
 let address = new Addresses();
 
-address.newAddress('men');
-address.newAddress('mmm');
+address.newAddress('men', 60);
+address.newAddress('mmm', 30);
 // console.log(address.chain);
-coin.createTransaction(new Transaction('08e3c4c91ca5c58e479ea699df7b1a7e3743831ac17b27e1e967c7d74f41df23', '8b7293f6dd93853c83bf8c87ec5349a41789fa166a9431f108edb538e6fd82fd', 6));
+coin.createTransaction(new Transaction(address.AD('08e3c4c91ca5c58e479ea699df7b1a7e3743831ac17b27e1e967c7d74f41df23'), address.AD('8b7293f6dd93853c83bf8c87ec5349a41789fa166a9431f108edb538e6fd82fd'), 6));
 coin.minePendingTransactions('miner');
-console.log(coin.getBalanceOfAddress('8b7293f6dd93853c83bf8c87ec5349a41789fa166a9431f108edb538e6fd82fd'));
+console.log(address.getBalanceOfAddress(address.AD('8b7293f6dd93853c83bf8c87ec5349a41789fa166a9431f108edb538e6fd82fd')));
